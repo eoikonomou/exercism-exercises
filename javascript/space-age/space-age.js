5d4af886a3f746a56596179ec8a9079d64929b05
Stathis Oikonomou <soikonomou@maseurope.com>
2019-02-25 13:12:19 +0200

    solved space-age exercise

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,35 @@
+/*
+Given an age in seconds, calculate how old someone would be on:
+
+   - Earth: orbital period 365.25 Earth days, or 31557600 seconds
+   - Mercury: orbital period 0.2408467 Earth years
+   - Venus: orbital period 0.61519726 Earth years
+   - Mars: orbital period 1.8808158 Earth years
+   - Jupiter: orbital period 11.862615 Earth years
+   - Saturn: orbital period 29.447498 Earth years
+   - Uranus: orbital period 84.016846 Earth years
+   - Neptune: orbital period 164.79132 Earth years
+
+So if you were told someone were 1,000,000,000 seconds old, you should
+be able to say that they're 31.69 Earth-years old.
+*/
+
+
+const EARTH_PERIOD = 31557600;
+
+const MULTIPLIERS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+const decimalConverter = (number, decimals) => Number(number.toFixed(decimals));
+
+const getOrbitalPeriod = planet => MULTIPLIERS[planet] * EARTH_PERIOD;
+
+export const age = (planet, seconds) => decimalConverter(seconds / getOrbitalPeriod(planet), 2);
